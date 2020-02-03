3d3255dafa4ec60983851267ab9cb63c8dc72cd1
Rich <rpavesi84@gmail.com>
2020-02-03 13:36:41 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,28 +1,27 @@
-// add code snippets from README
-let state
-
-function reducer(state={count:0}, action){
-    switch(action.type){
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-        default:
-          return state
-    }
+let state;
+function reducer(state = {count: 0}, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1}
+    default:
+      return state;
+  }
 }
-function dispatch (action {
-  state = reducer(state,action)
-  render()
-})
+
+function dispatch(action){
+  state = reducer(state, action);
+  render();
+}
 
 function render(){
-  let container = document.getElementById('container')
-  container.textConent = state.count
+  let container = document.getElementById('container');
+  container.textContent = state.count;
 }
 
 dispatch({type: '@@INIT'})
 
-let button = docuemtn.getElementById('button')
+let button = document.getElementById('button');
 
-button.addEventListener('click', () =>{
-  dispatch(type: 'INCREASE_COUNT')
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
 })
